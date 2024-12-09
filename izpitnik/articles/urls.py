3a16897d312f3051bbbee1429c8346c009df94b9
Borislav Angelov <sauronthegreat76@gmail.com>
2024-12-09 15:26:05 +0200

    feature: completed add/edit/create/article functionality, updated fixtures

--- izpitnik/articles/urls.py
+++ izpitnik/articles/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 
 from izpitnik.articles.views import ArticleListView, ArticlesOnDate, ArticleView, ArticlesByUser, ArticleAddView, \
-    ArticleDeleteView
+    ArticleDeleteView, ArticleEditView
 
 urlpatterns = [
     # path('', ArticleListView.as_view()),
     path('', ArticleListView.as_view(), name='all-articles'),
     path('<int:pk>/', include([
+        path('delete/', ArticleDeleteView.as_view(), name='article-delete'),
+        path('edit/', ArticleEditView.as_view(), name='article-edit'),
         path('<str:title>/', ArticleView.as_view(), name='single-article'),
-        path('delete/', ArticleDeleteView.as_view(), name='article-create'),
     ])),
     path('user_articles/', ArticlesByUser.as_view(), name='user-articles'),
     path('create/', ArticleAddView.as_view(), name='article-create'),
