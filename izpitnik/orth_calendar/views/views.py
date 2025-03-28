333fe00df2cac16f3ad36493a4f3b36f18c3c27d
b-angelov <sauronthegreat76@gmail.com>
2025-03-28 15:11:43 +0200

    feature: api js fetcher

--- izpitnik/orth_calendar/views/views.py
+++ izpitnik/orth_calendar/views/views.py
@@ -3,3 +3,16 @@ from django.views.generic import TemplateView
 
 class OrthodoxCalendarView(TemplateView):
     pass
+
+class OrthodoxApiJsView(TemplateView):
+    template_name = "orth_calendar/js/calendar_fetchery.js"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['api_url'] = self.request.build_absolute_uri('/') + 'orth_calendar'
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        response = super().render_to_response(context, **response_kwargs)
+        response['Content-Type'] = 'application/javascript'
+        return response
